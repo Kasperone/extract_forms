75f94eeedf54be48530d6f3c801b91102b6dd796
kali-ninja <kali-ninja@kali-ninja.kali-ninja>
2025-08-04 23:26:43 +0200

    wip c1

--- extract_forms.py
+++ extract_forms.py
@@ -1,1 +1,36 @@
 #! /usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+def request(url):
+    try:
+        return requests.get(url)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        pass
+    
+target_url = "http://192.168.0.44/mutillidae/index.php?page=dns-lookup.php"
+response = request(target_url)
+
+parsed_html = BeautifulSoup(response.content, "html.parser")
+forms_list = parsed_html.findAll("form")
+
+for form in forms_list:
+    action = form.get("action")
+    post_url = urljoin(target_url, action)
+    method = form.get("method")
+
+    inputs_list = form.findAll("input")
+    post_data = {}
+    for input in inputs_list:
+        input_name = input.get("name")
+        input_type = input.get("type")
+        input_value = input.get("value")
+        if input_type == "text":
+            input_value = "test"
+        post_data[input_name] = input_value
+
+    result = requests.post(post_url, data=post_data)
+    print(result.text)
